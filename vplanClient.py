9b42fc895d908d6e998637806aa576630c7deaf3
Lukas Schreiner <lukas.schreiner@fls-wiesbaden.de>
2012-01-29 20:48:16 +0000

    F11: Fullscreen an/aus eingebaut

--- vplanClient.py
+++ vplanClient.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from browser import *
+from Printer import Printer
+from PyQt4.QtCore import QObject, pyqtSignal, pyqtSlot
 import sys
 
 class VPlanMainWindow(QtGui.QMainWindow):
@@ -14,10 +16,23 @@ class VPlanMainWindow(QtGui.QMainWindow):
 
         self.addAction(self.exit)
 
+        self.fullScreen = QtGui.QAction(QtGui.QIcon(''), 'Vollbild an/aus', self)
+        self.fullScreen.setShortcut('F11')
+        self.fullScreen.setStatusTip('Vollbildmodus an/aus')
+        self.connect(self.fullScreen, QtCore.SIGNAL('triggered()'), QtCore.SLOT('toggleScreen()'))
+
+        self.addAction(self.fullScreen)
+
+    @pyqtSlot()
+    def toggleScreen(self):
+        if self.isFullScreen():
+            self.showNormal()
+        else:
+            self.showFullScreen()
+
 app = QtGui.QApplication(sys.argv)
 MainWindow = VPlanMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
-#MainWindow.show()
 MainWindow.showFullScreen()
 sys.exit(app.exec_())
